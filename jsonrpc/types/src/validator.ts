3900f1a4961e74bf36579934c67d33b6d8087880
Pedro Gomes <pedrogomes94@gmail.com>
2021-07-24 16:47:42 +0200

    jsonrpc validator types

---
+++ jsonrpc/types/src/validator.ts
@@ -0,0 +1,17 @@
+import { JsonRpcPayload } from "./jsonrpc";
+
+export interface JsonRpcValidationResult {
+  valid: boolean;
+  error?: string;
+}
+
+export interface JsonRpcValidationValid extends JsonRpcValidationResult {
+  valid: true;
+}
+
+export interface JsonRpcValidationInvalid extends JsonRpcValidationResult {
+  valid: false;
+  error: string;
+}
+
+export type JsonRpcValidation = JsonRpcValidationValid | JsonRpcValidationInvalid;
