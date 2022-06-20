42abf4dee08f6a40bec37cabc11fcc3f5a154991
Gancho Radkov <ganchoradkov93@gmail.com>
2022-06-20 11:52:32 +0300

    refactor(relay-api): restores `waku` methods for backwards compatibility

--- misc/relay-api/src/jsonrpc.ts
+++ misc/relay-api/src/jsonrpc.ts
@@ -1,6 +1,15 @@
 import { RelayJsonRpc } from "./types";
 
 export const RELAY_JSONRPC: { [protocol: string]: RelayJsonRpc.Methods } = {
+  waku: {
+    info: "waku_info",
+    connect: "waku_connect",
+    disconnect: "waku_disconnect",
+    publish: "waku_publish",
+    subscribe: "waku_subscribe",
+    subscription: "waku_subscription",
+    unsubscribe: "waku_unsubscribe",
+  },
   iridium: {
     info: "iridium_info",
     connect: "iridium_connect",
