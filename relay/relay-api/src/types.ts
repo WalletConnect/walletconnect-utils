377c3fbd2e7a7717b6650cb149765b204fc8984b
Pedro Gomes <pedrogomes94@gmail.com>
2022-06-27 12:44:41 +0200

    remove unused methods

--- relay/relay-api/src/types.ts
+++ relay/relay-api/src/types.ts
@@ -1,8 +1,5 @@
 export declare namespace RelayJsonRpc {
   export interface Methods {
-    info: string;
-    connect: string;
-    disconnect: string;
     publish: string;
     subscribe: string;
     subscription: string;
