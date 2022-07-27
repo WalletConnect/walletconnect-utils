b25449d03e53fce9fb8ca117a63e0fb8fb80bab1
Gancho Radkov <ganchoradkov93@gmail.com>
2022-07-27 10:30:41 +0300

    feat: adds `irn_` prefix to jsonrpc methods

--- relay/relay-api/src/jsonrpc.ts
+++ relay/relay-api/src/jsonrpc.ts
@@ -7,6 +7,12 @@ export const RELAY_JSONRPC: { [protocol: string]: RelayJsonRpc.Methods } = {
     subscription: "waku_subscription",
     unsubscribe: "waku_unsubscribe",
   },
+  irn: {
+    publish: "irn_publish",
+    subscribe: "irn_subscribe",
+    subscription: "irn_subscription",
+    unsubscribe: "irn_unsubscribe",
+  },
   iridium: {
     publish: "iridium_publish",
     subscribe: "iridium_subscribe",
