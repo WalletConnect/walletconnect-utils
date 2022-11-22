d5e3960e6a560e8b237a5113f2452cdf15ded3ed
Ben Kremer <contact@bkrem.dev>
2022-11-22 16:21:32 +0100

    fix(eslint): default as syntax in misc/logger

--- misc/logger/src/index.ts
+++ misc/logger/src/index.ts
@@ -1,5 +1,6 @@
+/* eslint-disable-next-line import/no-named-default */
+import { default as pino } from "pino";
 export * from "./constants";
 export * from "./utils";
 export type { Logger } from "pino";
-import { default as pino } from "pino";
 export { pino };
