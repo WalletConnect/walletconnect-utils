0d1f453e01898d052a9aa1b7dca4f0a5f7e67976
Celine Sarafa <celinesarafa@gmail.com>
2023-03-09 16:02:39 +0300

    Add cacao utils

---
+++ misc/cacao/src/utils.ts
@@ -0,0 +1,149 @@
+import { CacaoPayload, CacaoSignature } from "./types";
+import { hashMessage } from "@ethersproject/hash";
+import { recoverAddress } from "@ethersproject/transactions";
+import fetch from "isomorphic-unfetch";
+
+export const DEFAULT_RPC_URL = "https://rpc.walletconnect.com/v1";
+
+export const getDidAddressSegments = (iss: string) => {
+  return iss?.split(":");
+};
+
+export const getDidChainId = (iss: string) => {
+  const segments = iss && getDidAddressSegments(iss);
+  if (segments) {
+    return segments[3];
+  }
+  return undefined;
+};
+
+export const getNamespacedDidChainId = (iss: string) => {
+  const segments = iss && getDidAddressSegments(iss);
+  if (segments) {
+    return segments[2] + ":" + segments[3];
+  }
+  return undefined;
+};
+
+export const getDidAddress = (iss: string) => {
+  const segments = iss && getDidAddressSegments(iss);
+  if (segments) {
+    return segments.pop();
+  }
+  return undefined;
+};
+
+export const formatMessage = (cacao: CacaoPayload, iss: string) => {
+  const header = `${cacao.domain} wants you to sign in with your Ethereum account:`;
+  const walletAddress = getDidAddress(iss);
+  const statement = cacao.statement;
+  const uri = `URI: ${cacao.aud}`;
+  const version = `Version: ${cacao.version}`;
+  const chainId = `Chain ID: ${getDidChainId(iss)}`;
+  const nonce = `Nonce: ${cacao.nonce}`;
+  const issuedAt = `Issued At: ${cacao.iat}`;
+  const resources =
+    cacao.resources && cacao.resources.length > 0
+      ? `Resources:\n${cacao.resources.map((resource) => `- ${resource}`).join("\n")}`
+      : undefined;
+
+  const message = [
+    header,
+    walletAddress,
+    ``,
+    statement,
+    ``,
+    uri,
+    version,
+    chainId,
+    nonce,
+    issuedAt,
+    resources,
+  ]
+    .filter((val) => val !== undefined && val !== null) // remove unnecessary empty lines
+    .join("\n");
+
+  return message;
+};
+
+function isValidEip191Signature(address: string, message: string, signature: string): boolean {
+  const recoveredAddress = recoverAddress(hashMessage(message), signature);
+  return recoveredAddress.toLowerCase() === address.toLowerCase();
+}
+
+async function isValidEip1271Signature(
+  address: string,
+  reconstructedMessage: string,
+  signature: string,
+  chainId: string,
+  projectId: string,
+  rpcUrl?: string,
+) {
+  try {
+    const eip1271MagicValue = "0x1626ba7e";
+    const dynamicTypeOffset = "0000000000000000000000000000000000000000000000000000000000000040";
+    const dynamicTypeLength = "0000000000000000000000000000000000000000000000000000000000000041";
+    const nonPrefixedSignature = signature.substring(2);
+    const nonPrefixedHashedMessage = hashMessage(reconstructedMessage).substring(2);
+
+    const data =
+      eip1271MagicValue +
+      nonPrefixedHashedMessage +
+      dynamicTypeOffset +
+      dynamicTypeLength +
+      nonPrefixedSignature;
+
+    const response = await fetch(
+      `${rpcUrl ?? DEFAULT_RPC_URL}/?chainId=${chainId}&projectId=${projectId}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          id: generateJsonRpcId(),
+          jsonrpc: "2.0",
+          method: "eth_call",
+          params: [{ to: address, data }, "latest"],
+        }),
+      },
+    );
+    const { result } = await response.json();
+
+    if (!result) return false;
+
+    // Remove right-padded zeros from result to get only the concrete recovered value.
+    const recoveredValue = result.slice(0, eip1271MagicValue.length);
+    return recoveredValue.toLowerCase() === eip1271MagicValue.toLowerCase();
+  } catch (error: any) {
+    console.error("isValidEip1271Signature: ", error);
+    return false;
+  }
+}
+
+function generateJsonRpcId() {
+  return Date.now() + Math.floor(Math.random() * 1000);
+}
+
+export async function verifySignature(
+  address: string,
+  reconstructedMessage: string,
+  cacaoSignature: CacaoSignature,
+  chainId: string,
+  projectId: string,
+): Promise<boolean> {
+  // Determine if this signature is from an EOA or a contract.
+  switch (cacaoSignature.t) {
+    case "eip191":
+      return isValidEip191Signature(address, reconstructedMessage, cacaoSignature.s);
+    case "eip1271":
+      return await isValidEip1271Signature(
+        address,
+        reconstructedMessage,
+        cacaoSignature.s,
+        chainId,
+        projectId,
+      );
+    default:
+      throw new Error(
+        `verifySignature failed: Attempted to verify CacaoSignature with unknown type: ${cacaoSignature.t}`,
+      );
+  }
+}
