9531f6e719defb4cecca7c958938bf10987d067f
Cali93 <armut.cumaali@gmail.com>
2023-05-26 10:14:14 +0300

    chore(did-jwt): use latest did-jwt build

--- misc/identity-keys/src/identity-keys.ts
+++ misc/identity-keys/src/identity-keys.ts
@@ -10,7 +10,7 @@ import {
   generateJWT,
   jwtExp,
   JwtPayload,
-} from "@walletconnect/did-jwt/dist/esm";
+} from "@walletconnect/did-jwt";
 import { ICore, IStore } from "@walletconnect/types";
 import { formatMessage, generateRandomBytes32 } from "@walletconnect/utils";
 import axios from "axios";
