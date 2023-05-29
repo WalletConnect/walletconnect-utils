b9893c0b0a98fa98d4ea8b1cd26cf666a72b5c58
Celine Sarafa <celinesarafa@gmail.com>
2023-05-29 14:53:47 +0300

    Update misc/history/webpack.config.js
    Co-authored-by: Ben Kremer <ben@walletconnect.com>

--- misc/history/webpack.config.js
+++ misc/history/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist", "umd"),
     filename: "[name].min.js",
     libraryTarget: "umd",
-    library: "history",
+    library: "walletConnectHistoryUtil",
     umdNamedDefine: true,
     globalObject: "this",
   },
