7080d87f53a281162430ef40cc958bdbbfb4a7d0
Cali93 <armut.cumaali@gmail.com>
2023-05-29 16:15:27 +0300

    chore(downgrade-ed25519): downgrade @noble/ed25519

--- misc/did-jwt/src/did-jwt.ts
+++ misc/did-jwt/src/did-jwt.ts
@@ -1,5 +1,4 @@
 import * as ed25519 from "@noble/ed25519";
-import { sha512 } from "@noble/hashes/sha512";
 import { decode, encode } from "@stablelib/hex";
 import bs58 from "bs58";
 import { base58btc } from "multiformats/bases/base58";
@@ -15,7 +14,6 @@ import {
 } from "./constants";
 import { concatUInt8Arrays, makeBase64UrlSafe, objectToHex } from "./helpers";
 import { JwtHeader, JwtPayload } from "./types";
-ed25519.etc.sha512Sync = (...m) => sha512(ed25519.etc.concatBytes(...m));
 
 export const composeDidPkh = (accountId: string) => {
   return `${DID_PREFIX}${DID_DELIMITER}${DID_METHOD_PKH}${DID_DELIMITER}${accountId}`;
@@ -88,7 +86,7 @@ export const generateJWT = async (identityKeyPair: [string, string], payload: Jw
   const data = new TextEncoder().encode(encodeData(header, payload));
 
   console.log("did-jwt > generating signature");
-  const signature = ed25519.sign(encode(data), privateKey);
+  const signature = await ed25519.sign(encode(data), privateKey);
   console.log("Signature generated using ed25519.sign(encode(data), privateKey)");
   return encodeJwt(header, payload, signature);
   // try {
