65276835d17229f1e2b3a07376f4e8da04c371a3
Celine Sarafa <celinesarafa@gmail.com>
2023-05-30 16:03:09 +0300

    Add history utils (#84)
    * Add history utils
    
    * Update JWT and injection
    
    * Add test file
    
    * Update UTs
    
    * Update authentication
    
    * Add wait
    
    * Add longer wait to accomadate for ci
    
    * Add longer wait to prevent ci failing
    
    * Add smarter wait
    
    * Less strict ci
    
    * Remove extra wait
    
    * Remove jsonwebtoken
    
    * Update misc/history/src/utils.ts
    
    Co-authored-by: Ben Kremer <ben@walletconnect.com>
    
    * Update misc/history/src/utils.ts
    
    Co-authored-by: Ben Kremer <ben@walletconnect.com>
    
    * Update misc/history/webpack.config.js
    
    Co-authored-by: Ben Kremer <ben@walletconnect.com>
    
    * Remove unused timeout
    
    * Remove comment
    
    ---------
    
    Co-authored-by: Ben Kremer <ben@walletconnect.com>

---
+++ misc/history/src/types.ts
@@ -0,0 +1,27 @@
+export type Direction = "forward" | "backward";
+
+export interface RegisterPayload {
+  tags: string[];
+  relayUrl: string;
+}
+
+export interface GetMessagesPayload {
+  topic: string;
+  originId?: number;
+  messageCount?: number;
+  direction?: Direction;
+}
+
+export interface Message {
+  message: string;
+  message_id: string;
+  topic: string;
+  client_id: string;
+}
+
+export interface GetMessagesResponse {
+  topic: string;
+  direction: Direction;
+  nextId: number;
+  messages: Message[];
+}
---
+++ misc/history/src/utils.ts
@@ -0,0 +1,68 @@
+import { RegisterPayload, GetMessagesPayload, GetMessagesResponse } from "./types";
+import fetch from "isomorphic-unfetch";
+import type { ICore } from "@walletconnect/types";
+
+export const DEFAULT_HISTORY_URL = "https://history.walletconnect.com";
+
+export const DAY_IN_MS = 86400 * 1000;
+
+export class HistoricalMessages {
+  public constructor(private core: ICore, public messageResponse: GetMessagesResponse) {}
+
+  public injectIntoRelayer() {
+    for (const message in this.messageResponse.messages) {
+      this.core.relayer.provider.events.emit("payload", message);
+    }
+  }
+}
+
+export class HistoryClient {
+  private jwt = "";
+
+  public constructor(private core: ICore, private readonly historyUrl = DEFAULT_HISTORY_URL) {}
+
+  public async registerTags(payload: RegisterPayload, historyUrl = DEFAULT_HISTORY_URL) {
+    try {
+      await fetch(`${historyUrl}/register`, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${await this.getJwt()}`
+        },
+      });
+
+    } catch (e) {
+      throw new Error(`[HistoryClient] Failed to register tags: ${JSON.stringify(e)}`);
+    }
+  }
+
+  public async getMessages(payload: GetMessagesPayload, historyUrl = DEFAULT_HISTORY_URL) {
+    const entries = Object.entries(payload).map(([key, value]) => [key, value.toString()]);
+    const params = new URLSearchParams(entries);
+
+    try {
+      const url = `${historyUrl}/messages?${params.toString()}`
+      const rs: GetMessagesResponse = await (
+        await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+      ).json();
+
+      return new HistoricalMessages(this.core, rs);
+    } catch (e) {
+      throw new Error(`[HistoryClient] Failed to fetch messages: ${JSON.stringify(e)}`);
+    }
+  }
+
+  public async getJwt() {
+    if (this.jwt) return this.jwt;
+
+    this.jwt = await this.core.crypto.signJWT(this.historyUrl);
+
+    return this.jwt;
+  }
+}
