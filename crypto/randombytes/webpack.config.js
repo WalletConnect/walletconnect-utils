f9225177f9c99ae13d8f2e4fb139bd72358403fd
Ben Kremer <ben@walletconnect.com>
2023-06-01 14:36:12 +0200

    fix: restore `@walletconnect/crypto` build chain due to env specifics

---
+++ crypto/randombytes/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: path.resolve(__dirname, 'dist', 'cjs', 'fallback', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist', 'umd'),
+    filename: '[name].min.js',
+    libraryTarget: 'umd',
+    library: 'RandomBytes',
+    umdNamedDefine: true,
+    globalObject: 'this',
+  },
+};
