c1b472f7d40808c0ec9d5547ad357b31a4533fb2
Celine Sarafa <celinesarafa@gmail.com>
2023-06-12 14:29:41 +0300

    Store messages to check against (#110)

--- misc/history/src/utils.ts
+++ misc/history/src/utils.ts
@@ -7,20 +7,22 @@ export const DEFAULT_HISTORY_URL = "https://history.walletconnect.com";
 export const DAY_IN_MS = 86400 * 1000;
 
 export class HistoricalMessages {
+  // eslint-disable-next-line no-useless-constructor
   public constructor(private core: ICore, public messageResponse: GetMessagesResponse) {}
 
   public async injectIntoRelayer() {
     const { messages, topic } = this.messageResponse;
-    for (const message of messages) {
-
-      if(this.core.relayer.messages.has(topic, message.message)){
+    for (const { message } of messages) {
+      if (this.core.relayer.messages.has(topic, message)) {
         continue;
       }
+
+      await this.core.relayer.messages.set(topic, message);
 
       this.core.relayer.events.emit("relayer_message", {
         topic,
         publishedAt: Date.now(),
-        message: message.message,
+        message,
       });
     }
   }
@@ -29,6 +31,7 @@ export class HistoricalMessages {
 export class HistoryClient {
   private jwt = "";
 
+  // eslint-disable-next-line no-useless-constructor
   public constructor(private core: ICore, private readonly historyUrl = DEFAULT_HISTORY_URL) {}
 
   public async registerTags(payload: RegisterPayload, historyUrl = DEFAULT_HISTORY_URL) {
@@ -41,7 +44,6 @@ export class HistoryClient {
           Authorization: `Bearer ${await this.getJwt()}`,
         },
       });
-
     } catch (e) {
       throw new Error(`[HistoryClient] Failed to register tags: ${JSON.stringify(e)}`);
     }
