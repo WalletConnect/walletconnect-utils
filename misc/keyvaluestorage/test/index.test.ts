7c045199809e53bec5cd20e64fab2e54838b7576
Gancho Radkov <ganchoradkov@gmail.com>
2023-10-12 14:39:17 +0300

    refactor: updates test lints

--- misc/keyvaluestorage/test/index.test.ts
+++ misc/keyvaluestorage/test/index.test.ts
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-expressions */
 // @ts-nocheck
-import "mocha";
-import * as chai from "chai";
+import { describe, it, before } from "mocha";
+import { expect } from "chai";
 import proxyquire from "proxyquire";
 import { delay } from "@walletconnect/time";
 
@@ -34,27 +35,27 @@ describe("KeyValueStorage", () => {
     it("getItem", async () => {
       const item = await storage.getItem<typeof value>(key);
       if (typeof item === "undefined") throw new Error("item is missing");
-      chai.expect(item).to.not.be.undefined;
-      chai.expect(item.name).to.not.be.undefined;
-      chai.expect(item.name).to.eql(value.name);
+      expect(item).to.not.be.undefined;
+      expect(item.name).to.not.be.undefined;
+      expect(item.name).to.eql(value.name);
     });
     it("getEntries", async () => {
       const entries = await storage.getEntries();
-      chai.expect(entries).to.not.be.undefined;
-      chai.expect(entries.length).to.eql(1);
-      chai.expect(entries[0]).to.eql([key, value]);
+      expect(entries).to.not.be.undefined;
+      expect(entries.length).to.eql(1);
+      expect(entries[0]).to.eql([key, value]);
     });
     it("getKeys", async () => {
       const keys = await storage.getKeys();
-      chai.expect(keys).to.not.be.undefined;
-      chai.expect(keys.length).to.eql(1);
-      chai.expect(keys[0]).to.eql(key);
+      expect(keys).to.not.be.undefined;
+      expect(keys.length).to.eql(1);
+      expect(keys[0]).to.eql(key);
     });
     it("removeItem", async () => {
       await storage.removeItem(key);
       const item = await storage.getItem(key);
       if (typeof item !== "undefined") throw new Error("item expected to be undefined");
-      chai.expect(!item).to.be.true;
+      expect(!item).to.be.true;
     });
   });
 
@@ -66,27 +67,27 @@ describe("KeyValueStorage", () => {
     it("getItem", async () => {
       const item = await storage.getItem<typeof value>(key);
       if (typeof item === "undefined") throw new Error("item is missing");
-      chai.expect(item).to.not.be.undefined;
-      chai.expect(item.name).to.not.be.undefined;
-      chai.expect(item.name).to.eql(value.name);
+      expect(item).to.not.be.undefined;
+      expect(item.name).to.not.be.undefined;
+      expect(item.name).to.eql(value.name);
     });
     it("getEntries", async () => {
       const entries = await storage.getEntries();
-      chai.expect(entries).to.not.be.undefined;
-      chai.expect(entries.length).to.eql(1);
-      chai.expect(entries[0]).to.eql([key, value]);
+      expect(entries).to.not.be.undefined;
+      expect(entries.length).to.eql(1);
+      expect(entries[0]).to.eql([key, value]);
     });
     it("getKeys", async () => {
       const keys = await storage.getKeys();
-      chai.expect(keys).to.not.be.undefined;
-      chai.expect(keys.length).to.eql(1);
-      chai.expect(keys[0]).to.eql(key);
+      expect(keys).to.not.be.undefined;
+      expect(keys.length).to.eql(1);
+      expect(keys[0]).to.eql(key);
     });
     it("removeItem", async () => {
       await storage.removeItem(key);
       const item = await storage.getItem(key);
       if (typeof item !== "undefined") throw new Error("item expected to be undefined");
-      chai.expect(!item).to.be.true;
+      expect(!item).to.be.true;
     });
   });
 
@@ -98,27 +99,27 @@ describe("KeyValueStorage", () => {
     it("getItem", async () => {
       const item = await storage.getItem<typeof value>(key);
       if (typeof item === "undefined") throw new Error("item is missing");
-      chai.expect(item).to.not.be.undefined;
-      chai.expect(item.name).to.not.be.undefined;
-      chai.expect(item.name).to.eql(value.name);
+      expect(item).to.not.be.undefined;
+      expect(item.name).to.not.be.undefined;
+      expect(item.name).to.eql(value.name);
     });
     it("getEntries", async () => {
       const entries = await storage.getEntries();
-      chai.expect(entries).to.not.be.undefined;
-      chai.expect(entries.length).to.eql(1);
-      chai.expect(entries[0]).to.eql([key, value]);
+      expect(entries).to.not.be.undefined;
+      expect(entries.length).to.eql(1);
+      expect(entries[0]).to.eql([key, value]);
     });
     it("getKeys", async () => {
       const keys = await storage.getKeys();
-      chai.expect(keys).to.not.be.undefined;
-      chai.expect(keys.length).to.eql(1);
-      chai.expect(keys[0]).to.eql(key);
+      expect(keys).to.not.be.undefined;
+      expect(keys.length).to.eql(1);
+      expect(keys[0]).to.eql(key);
     });
     it("removeItem", async () => {
       await storage.removeItem(key);
       const item = await storage.getItem(key);
       if (typeof item !== "undefined") throw new Error("item expected to be undefined");
-      chai.expect(!item).to.be.true;
+      expect(!item).to.be.true;
     });
   });
 
@@ -128,19 +129,20 @@ describe("KeyValueStorage", () => {
         database: PERSISTED_NODEJS_DATABASE,
       });
       await storageA.setItem(key, value);
+      await storageA.setItem(key, value);
       const itemA = await storageA.getItem<typeof value>(key);
       if (typeof itemA === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemA).to.not.be.undefined;
-      chai.expect(itemA.name).to.not.be.undefined;
-      chai.expect(itemA.name).to.eql(value.name);
+      expect(itemA).to.not.be.undefined;
+      expect(itemA.name).to.not.be.undefined;
+      expect(itemA.name).to.eql(value.name);
       const storageB = new NodeJSStorage({
         database: PERSISTED_NODEJS_DATABASE,
       });
       const itemB = await storageB.getItem<typeof value>(key);
       if (typeof itemB === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemB).to.not.be.undefined;
-      chai.expect(itemB.name).to.not.be.undefined;
-      chai.expect(itemB.name).to.eql(value.name);
+      expect(itemB).to.not.be.undefined;
+      expect(itemB.name).to.not.be.undefined;
+      expect(itemB.name).to.eql(value.name);
     });
     it("two classes can share the same storage", async () => {
       const storage = new NodeJSStorage({
@@ -150,15 +152,15 @@ describe("KeyValueStorage", () => {
       await storeA.set(key, value);
       const itemA = await storeA.get<typeof value>(key);
       if (typeof itemA === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemA).to.not.be.undefined;
-      chai.expect(itemA.name).to.not.be.undefined;
-      chai.expect(itemA.name).to.eql(value.name);
+      expect(itemA).to.not.be.undefined;
+      expect(itemA.name).to.not.be.undefined;
+      expect(itemA.name).to.eql(value.name);
       const storeB = new MockStore(storage);
       const itemB = await storeB.get<typeof value>(key);
       if (typeof itemB === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemB).to.not.be.undefined;
-      chai.expect(itemB.name).to.not.be.undefined;
-      chai.expect(itemB.name).to.eql(value.name);
+      expect(itemB).to.not.be.undefined;
+      expect(itemB.name).to.not.be.undefined;
+      expect(itemB.name).to.eql(value.name);
     });
     it("three storages can write synchronously", async () => {
       const storageA = new NodeJSStorage({
@@ -181,24 +183,24 @@ describe("KeyValueStorage", () => {
       const itemA = await storageA.getItem<typeof value>(key);
 
       if (typeof itemA === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemA).to.not.be.undefined;
-      chai.expect(itemA.name).to.not.be.undefined;
-      chai.expect(itemA.name).to.eql(value.name);
-      chai.expect((itemA as any).owner).to.eql("storageC");
+      expect(itemA).to.not.be.undefined;
+      expect(itemA.name).to.not.be.undefined;
+      expect(itemA.name).to.eql(value.name);
+      expect((itemA as any).owner).to.eql("storageC");
 
       const itemB = await storageB.getItem<typeof value>(key);
       if (typeof itemB === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemB).to.not.be.undefined;
-      chai.expect(itemB.name).to.not.be.undefined;
-      chai.expect(itemB.name).to.eql(value.name);
-      chai.expect((itemB as any).owner).to.eql("storageC");
+      expect(itemB).to.not.be.undefined;
+      expect(itemB.name).to.not.be.undefined;
+      expect(itemB.name).to.eql(value.name);
+      expect((itemB as any).owner).to.eql("storageC");
 
       const itemC = await storageB.getItem<typeof value>(key);
       if (typeof itemC === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemC).to.not.be.undefined;
-      chai.expect(itemC.name).to.not.be.undefined;
-      chai.expect(itemC.name).to.eql(value.name);
-      chai.expect((itemC as any).owner).to.eql("storageC");
+      expect(itemC).to.not.be.undefined;
+      expect(itemC.name).to.not.be.undefined;
+      expect(itemC.name).to.eql(value.name);
+      expect((itemC as any).owner).to.eql("storageC");
     });
     it("can create multiple persisted DBs", async () => {
       const dbA = `${dbDir}/a.db`;
@@ -219,17 +221,17 @@ describe("KeyValueStorage", () => {
       const itemA = await storageA.getItem<typeof value>(key);
 
       if (typeof itemA === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemA).to.not.be.undefined;
-      chai.expect(itemA.name).to.not.be.undefined;
-      chai.expect(itemA.name).to.eql(value.name);
-      chai.expect((itemA as any).owner).to.eql("storageA");
+      expect(itemA).to.not.be.undefined;
+      expect(itemA.name).to.not.be.undefined;
+      expect(itemA.name).to.eql(value.name);
+      expect((itemA as any).owner).to.eql("storageA");
 
       const itemB = await storageB.getItem<typeof value>(key);
       if (typeof itemB === "undefined") throw new Error("item expected to be undefined");
-      chai.expect(itemB).to.not.be.undefined;
-      chai.expect(itemB.name).to.not.be.undefined;
-      chai.expect(itemB.name).to.eql(value.name);
-      chai.expect((itemB as any).owner).to.eql("storageB");
+      expect(itemB).to.not.be.undefined;
+      expect(itemB.name).to.not.be.undefined;
+      expect(itemB.name).to.eql(value.name);
+      expect((itemB as any).owner).to.eql("storageB");
     });
   });
 });
