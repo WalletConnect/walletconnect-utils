b976952bbb23d63f656fa7744007d9f4b9f405cd
Gancho Radkov <ganchoradkov@gmail.com>
2023-11-08 16:03:09 +0200

    chore: removes leftover logs

--- misc/keyvaluestorage/src/browser/lib/browserMigration.ts
+++ misc/keyvaluestorage/src/browser/lib/browserMigration.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { IKeyValueStorage } from "../../shared";
 
 const VERSION_KEY = "wc_storage_version";
@@ -9,18 +8,14 @@ export const migrate = async (
   toStore: IKeyValueStorage,
   callback: (store: IKeyValueStorage) => void,
 ) => {
-  console.log("🚢 migrate: start");
   const versionKey = VERSION_KEY;
   const currentVersion = await toStore.getItem<number>(versionKey);
   if (currentVersion && currentVersion >= DB_VERSION) {
-    console.log("🚢 migrate: already migrated");
     callback(toStore);
     return;
   }
-  console.log("🚢 migrate: start");
   const keys = await fromStore.getKeys();
   if (!keys.length) {
-    console.log("🚢 migrate: no keys");
     callback(toStore);
     return;
   }
@@ -38,27 +33,20 @@ export const migrate = async (
       const item = await fromStore.getItem(key);
       await toStore.setItem(key, item);
       keysToCleanup.push(key);
-    } else {
-      console.log("🚢 migrate: skipping key", key);
     }
   }
 
   await toStore.setItem(versionKey, DB_VERSION);
-  console.log("🚢 migrate: complete");
-  console.log("cleanup: keys", keysToCleanup.length);
   callback(toStore);
   cleanup(fromStore, keysToCleanup);
 };
 
 const cleanup = async (store: IKeyValueStorage, keysToCleanup: string[]) => {
   if (!keysToCleanup.length) {
-    console.log("🧹 cleanup: no keys", keysToCleanup.length);
     return;
   }
 
-  console.log("🧹 cleanup: start");
   keysToCleanup.forEach(async (key: string) => {
     await store.removeItem(key);
   });
-  console.log("🧹 cleanup: complete");
 };
