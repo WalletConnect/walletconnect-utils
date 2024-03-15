4588004d933f8b9eacc9c64cf6a16170c777bdee
Celine Sarafa <celinesarafa@gmail.com>
2024-03-15 11:07:02 +0100

    feat: add client and server array logger

---
+++ misc/logger/src/linkedList.ts
@@ -0,0 +1,83 @@
+class LogListNode {
+  private nodeValue: string;
+  private sizeInBytes: number;
+  public next: LogListNode | null;
+
+  constructor(value: string) {
+    this.nodeValue = value;
+    this.sizeInBytes = new TextEncoder().encode(value).length;
+    this.next = null;
+  }
+
+  public get value() {
+    return this.nodeValue;
+  }
+
+  public get size() {
+    return this.sizeInBytes;
+  }
+
+}
+
+export class LogLinkedList {
+  private lengthInNodes: number;
+  private sizeInBytes: number;
+  private head: LogListNode | null;
+  private tail: LogListNode | null;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.lengthInNodes = 0;
+    this.sizeInBytes = 0;
+  }
+
+  public append(value: string): void {
+    const newNode = new LogListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      if (this.tail) {
+          this.tail.next = newNode;
+      }
+      this.tail = newNode;
+    }
+    this.lengthInNodes++;
+    this.sizeInBytes += newNode.size;
+  }
+
+  public shift(): void {
+    if (!this.head) {
+      return;
+    }
+
+    const removedNode = this.head;
+    this.head = this.head.next;
+
+    if (!this.head) {
+      this.tail = null;
+    }
+
+    this.lengthInNodes--;
+    this.sizeInBytes -= removedNode.size
+  }
+
+  public toArray(): string[] {
+      const array: string[] = [];
+      let currentNode = this.head;
+      while (currentNode !== null) {
+          array.push(currentNode.value);
+          currentNode = currentNode.next;
+      }
+      return array;
+  }
+
+  public get length() {
+    return this.lengthInNodes;
+  }
+
+  public get size() {
+    return this.sizeInBytes;
+  }
+}
---
+++ misc/logger/src/serverArrayLogger.ts
@@ -0,0 +1,64 @@
+import { MAX_LOG_SIZE_IN_BYTES_DEFAULT } from "./constants";
+import { Writable } from 'stream'
+import { LogLinkedList } from "./linkedList";
+
+export class ServerArrayLogger extends Writable {
+  private logs: LogLinkedList
+  private MAX_LOG_SIZE_IN_BYTES: number; 
+
+  public constructor(
+    MAX_LOG_SIZE_IN_BYTES: number = MAX_LOG_SIZE_IN_BYTES_DEFAULT,
+  ) {
+    super({ objectMode: true })
+
+    this.MAX_LOG_SIZE_IN_BYTES = MAX_LOG_SIZE_IN_BYTES;
+    this.logs = new LogLinkedList()
+
+    // @ts-ignore
+    window.w3iLogger = this
+
+    // @ts-ignore
+    window.downloadLogsBlob = (clientMetadata?: LogClientMetadata) => {
+      this.downloadLogsBlobInBrowser(clientMetadata ?? { clientId: 'N/A' })
+      this.clearLogs()
+    }
+  }
+
+  public _write(chunk: any): void {
+    this.logs.append(
+      JSON.stringify({
+        timestamp: new Date().toISOString(),
+        log: chunk
+      })
+    )
+
+    if (this.logs.size >= this.MAX_LOG_SIZE_IN_BYTES) {
+      this.logs.shift()
+    }
+  }
+
+  public getLogs() {
+    return this.logs
+  }
+
+  public clearLogs() {
+    this.logs = new LogLinkedList()
+  }
+
+  public logsToBlob(extraMetadata: Record<string, string>) {
+    this.logs.append(JSON.stringify({ extraMetadata }))
+    const blob = new Blob(this.logs.toArray(), { type: 'application/json' })
+    return blob
+  }
+
+  public downloadLogsBlobInBrowser(extraMetadata: Record<string, string>) {
+    const url = URL.createObjectURL(this.logsToBlob(extraMetadata))
+    const anchor = document.createElement('a')
+    anchor.href = url
+    anchor.download = `w3i-logs-${new Date().toISOString()}.txt`
+    document.body.appendChild(anchor)
+    anchor.click()
+    document.body.removeChild(anchor)
+    URL.revokeObjectURL(url)
+  }
+}
