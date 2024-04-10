fcbcd12942e665b2a1fbcddcc3b7404a42422b94
everdimension <everdimension@gmail.com>
2024-04-10 13:34:57 +0800

    Improve payloadId generator

--- jsonrpc/utils/src/format.ts
+++ jsonrpc/utils/src/format.ts
@@ -2,9 +2,37 @@ import { getError, getErrorByCode, isReservedErrorCode } from "./error";
 import { INTERNAL_ERROR, SERVER_ERROR } from "./constants";
 import { ErrorResponse, JsonRpcError, JsonRpcRequest, JsonRpcResult } from "./types";
 
+class IncrementalRandomGenerator {
+  private typedArray: Uint8Array | Uint16Array | Uint32Array;
+  private shift: number;
+  private base = 1;
+
+  static getBitsOf(typedArray: Uint8Array | Uint16Array | Uint32Array) {
+    if (!typedArray.length) {
+      throw new Error("Empty typed array");
+    }
+    return (typedArray.byteLength / typedArray.length) * 8;
+  }
+
+  constructor(bits: 8 | 16 | 32) {
+    this.typedArray =
+      bits === 8 ? new Uint8Array(1) : bits === 16 ? new Uint16Array(1) : new Uint32Array(1);
+    this.shift = IncrementalRandomGenerator.getBitsOf(this.typedArray);
+  }
+
+  getRandomValue() {
+    return (this.base++ << this.shift) + crypto.getRandomValues(this.typedArray)[0];
+  }
+}
+
+const uint8Generator = new IncrementalRandomGenerator(8);
+const uint16Generator = new IncrementalRandomGenerator(16);
+
 export function payloadId(entropy = 3): number {
-  const date = Date.now() * Math.pow(10, entropy);
-  const extra = Math.floor(Math.random() * Math.pow(10, entropy));
+  const generator = entropy > 3 ? uint16Generator : uint8Generator;
+  const shift = entropy > 3 ? 10000 : 1000000;
+  const date = Date.now() * Math.pow(10, shift);
+  const extra = generator.getRandomValue();
   return date + extra;
 }
 
