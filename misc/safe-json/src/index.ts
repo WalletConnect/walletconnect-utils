851534e771dfd7f958a51f0f292dd188e860a5be
Bram Cohen <bram@chia.net>
2026-07-15 16:09:32 -0700

    Fixed bug where negative integers aren't parsed properly

--- misc/safe-json/src/index.ts
+++ misc/safe-json/src/index.ts
@@ -27,7 +27,7 @@ const JSONParse = (json: string) => {
   const numbersBiggerThanMaxInt = /(?<!")(?<=:)\b(\d{17,}|(?:[9](?:[1-9]07199254740991|0[1-9]7199254740991|00[8-9]199254740991|007[2-9]99254740991|007199[3-9]54740991|0071992[6-9]4740991|00719925[5-9]740991|007199254[8-9]40991|0071992547[5-9]0991|00719925474[1-9]991|00719925474099[2-9])))(?=[,\}\]]|$)/g;
   const serializedData = json.replace(numbersBiggerThanMaxInt, (match) => `"${match}n"`);
   return JSON.parse(serializedData, (_, value) => {
-    const isCustomFormatBigInt = typeof value === "string" && value.match(/^\d+n$/);
+    const isCustomFormatBigInt = typeof value === "string" && value.match(/^-?\d+n$/);
 
     if (isCustomFormatBigInt) return BigInt(value.substring(0, value.length - 1));
 
